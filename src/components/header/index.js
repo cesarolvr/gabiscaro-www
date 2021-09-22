bec8ac65ebe31c5343d43bb24d1ea18be02c193a
cesarolvr <cesar2012oliveira@gmail.com>
2021-09-21 21:58:15 -0300

    favicon added in project

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,24 +1,34 @@
 import * as React from "react";
 import { Link } from "gatsby";
+import { Helmet } from "react-helmet";
 
 import "./index.scss";
 
+import favicon from '../../images/favicon.svg';
+
+
 import Logo from "@components/Logo";
 
-const Header = ({className = ''}) => {
-    return (
-        <header className={`header ${className}`}>
-            <Link className="title" to="/illustrations">
-                illustrations
-            </Link>
-            <Link className="title" to="/">
-                <Logo />
-            </Link>
-            <Link className="link" to="/about">
-                about/say hi
-            </Link>
-        </header>
-    );
+const Header = ({ className = "" }) => {
+  return (
+    <header className={`header ${className}`}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Gabiscaro | Designer and illustrator</title>
+        <link rel="icon" type="image/png" href={favicon} sizes="16x16" />
+        
+      </Helmet>
+      <Link className="title" to="/illustrations">
+        illustrations
+      </Link>
+      <Link className="title" to="/">
+        <Logo />
+      </Link>
+      <Link className="link" to="/about">
+        about/say hi
+      </Link>
+    </header>
+  );
 };
 
 export default Header;
