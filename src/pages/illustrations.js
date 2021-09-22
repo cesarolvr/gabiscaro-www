da67bd5113c94709d9c58406467d3fe232d17643
cesarolvr <cesar2012oliveira@gmail.com>
2021-09-21 22:24:52 -0300

    assets of illustrations screen added

--- src/pages/illustrations.js
+++ src/pages/illustrations.js
@@ -1,24 +1,58 @@
 import * as React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 
 import Header from "@components/header";
 import Contact from "@components/contact";
 
+import "@styles/illustrations.scss";
+
 const Illustrations = () => {
-    return (
-        <>
-            <Header />
-            <br />
-            <br />
-            <br />
-            illustrations
-            <br />
-            <br />
-            <br />
-            <main>
-                <Contact />
-            </main>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <div className="grid">
+        <div className="case">
+          <StaticImage src="../images/1.png" />
+        </div>
+        <div className="case">
+          <StaticImage src="../images/greenstar.svg" />
+          <StaticImage src="../images/halfcircle.svg" />
+        </div>
+        <div className="case">
+          <StaticImage src="../images/2.png" />
+        </div>
+        <div className="case">
+          <StaticImage src="../images/3.png" />
+        </div>
+        <div className="case">
+          <StaticImage src="../images/4.png" />
+        </div>
+        <div className="case">
+          <StaticImage src="../images/5.png" />
+        </div>
+        <div className="case">
+          <StaticImage src="../images/flatcloud.svg" />
+        </div>
+        <div className="case">
+          <StaticImage src="../images/6.png" />
+        </div>
+        <div className="case">
+          <StaticImage src="../images/7.png" />
+        </div>
+        <div className="case">
+          <StaticImage src="../images/8.png" />
+        </div>
+        <div className="case">
+          <StaticImage src="../images/yellowsquare.svg" />
+          <StaticImage src="../images/greenpil.svg" />
+          <StaticImage src="../images/pinkcircle.svg" />
+        </div>
+      </div>
+      <main>
+        <Contact />
+      </main>
+    </>
+  );
 };
 
 export default Illustrations;
