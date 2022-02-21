fcccaf934e430dba3b52110228ea306161317fdb
cesarolvr <cesar2012oliveira@gmail.com>
2022-02-21 11:33:15 -0300

    new build

--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -6,7 +6,7 @@ const isClient = () => typeof window !== "undefined";
 const useScroll = (carouselMounted = false) => {
   useEffect(() => {
     let scroll = null;
-    if (!isClient) return;
+    if (!isClient()) return;
     setTimeout(() => {
       scroll = new LocomotiveScroll({
         el: document.querySelector(".scroll-content"),
