b22dc9505a5ec52a6da2e9b5216f98e6098f3ce5
cesarolvr <cesar2012oliveira@gmail.com>
2022-03-04 16:16:06 -0300

    description changed

--- src/html.js
+++ src/html.js
@@ -3,10 +3,14 @@ import PropTypes from "prop-types";
 
 export default function HTML(props) {
   return (
-    <html {...props.htmlAttributes}>
+    <html {...props.htmlAttributes} lang="pt-BR">
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="description"
+          content="I'm Gabriela Biscáro, a Product Designer and passionate Illustrator based in Sao Paulo, Brazil. E esse é meu portfólio pessoal."
+        />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
